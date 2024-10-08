3c6445fd54b6833d085f22c5ad2f97e4e60b24fb
Rory Jarrard <rjarrard@gmail.com>
2024-10-08 13:35:38 -0600

    fetch users or error

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,15 +1,25 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUsers } from '../store';
 
 const UsersList = () => {
     const dispatch = useDispatch();
 
+    const {data, isLoading, error} = useSelector((state) => state.users);
+
     useEffect(() => {
         dispatch(fetchUsers());
     }, [dispatch]);
 
-    return 'Users List';
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    }
+
+    return <div>{data.length}</div>;
 };
 
 export default UsersList;
--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -4,7 +4,13 @@ import axios from 'axios';
 const fetchUsers = createAsyncThunk('users/fetch', async () => {
     const response = await axios.get('http://localhost:3005/users');
 
+    // DEV ONLY!
+    await delay(2000);
+
     return response.data;
 });
+
+// DEV ONLY!
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export { fetchUsers };
