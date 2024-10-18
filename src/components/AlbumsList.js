26970c7386eb8cc40c2b5c32f14057fb5b2940e5
Rory Jarrard <rjarrard@gmail.com>
2024-10-18 12:50:37 -0600

    add diplay panel for albums

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,12 +1,29 @@
 import { useFetchAlbumsQuery } from '../store';
+import SkeletonLoader from './SkeletonLoader';
+import ExpandablePanel from './ExpandablePanel';
+import Button from './Button';
 
 const AlbumsList = ({ user }) => {
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data, error, isLoading);
+    let content = null;
+    if (isLoading) {
+        content = <SkeletonLoader times={3} />;
+    } else if (error) {
+        content = <div>Error loading albums.</div>;
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>;
+            return <ExpandablePanel key={album.id} header={header}>
+                List of photos for album {album.title}
+            </ExpandablePanel>;
+        });
+    }
 
-    return (
+    return (<div>
         <div>Albums for {user.name}</div>
+        <div>{content}</div>
+    </div>
     );
 };
 
