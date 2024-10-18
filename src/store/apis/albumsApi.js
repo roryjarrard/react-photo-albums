f8dd0a2b82345badfed3f6c609ce11b4538957cf
Rory Jarrard <rjarrard@gmail.com>
2024-10-18 14:23:55 -0600

    implement tags in api

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -7,6 +7,9 @@ const albumsApi = createApi({
     endpoints: (builder) => {
         return {
             fetchAlbums: builder.query({
+                providesTags: (result, error, user) => {
+                    return [{type: 'Album', id: user.id}];
+                },
                 query: (user) => {
                     return {
                         url: '/albums',
@@ -18,6 +21,9 @@ const albumsApi = createApi({
                 },
             }),
             addAlbum: builder.mutation({
+                invalidatesTags: (result, error, user) => {
+                    return [{type: 'Album', id: user.id}];
+                },
                 query: (user) => {
                     return {
                         url: '/albums',
