5b0e2435fdd020861e4ca67b4b2d3b70d16bc09d
Rory Jarrard <rjarrard@gmail.com>
2024-10-22 14:10:53 -0600

    remove photos

--- src/components/PhotosListItem.js
+++ src/components/PhotosListItem.js
@@ -1,7 +1,19 @@
-const PhotosListtItem = ({photo}) => {
+import { GoTrashcan } from 'react-icons/go';
+import { useRemovePhotoMutation } from '../store';
+
+const PhotosListtItem = ({ photo }) => {
+    const [removePhoto] = useRemovePhotoMutation();
+
+    const handleRemovePhoto = () => {
+        removePhoto(photo);
+    };
+
     return (
-        <div>
+        <div className="relative cursor-pointer m-2" onClick={handleRemovePhoto}>
             <img className="h-20 w-20" src={photo.url} alt="random pic" />
+            <div className="absolute inset-0 flex items-center justify-center hover:bg-gray-200 opacity-0 hover:opacity-80">
+                <GoTrashcan className="text-3xl" />
+            </div>
         </div>
     );
 };
